83b33e1e1b7e9ab9917f040f413a0c65172c682f
gaojie12345 <1768916170@qqcom>
2018-11-20 18:57:10 +0800

    提交代码

---
+++ 10.31 100~200素数/ConsoleApplication4/源.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i, j;
+	for (i = 100; i <= 200; i++)
+	{
+		for (j = 2; j <= i - 1; j++)
+		{
+			if (i%j == 0)
+				break;
+		}
+		{
+			if (j == i)
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//	int i, j;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		for (j = 2; j <= i - 1; j++)
+//		{
+//			if (i %j == 0)
+//				break;
+//		}
+//		{
+//			if (j == i)
+//				printf("%d ", i);
+//		}
+//	}
+//	printf("\n");
+//		system("pause");
+//		return 0;
+//}
